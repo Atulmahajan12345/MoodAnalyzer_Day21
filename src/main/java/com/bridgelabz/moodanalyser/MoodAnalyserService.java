48ecccd937b25c20037116969c90d8c0655610bb
Atulmahajan12345 <atulmahajan2299@gmail.com>
2022-04-14 14:34:45 +0530

    test case when no mood specified should return happy

--- src/main/java/com/bridgelabz/moodanalyser/MoodAnalyserService.java
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyserService.java
@@ -2,25 +2,7 @@ package com.bridgelabz.moodanalyser;
 
 public class MoodAnalyserService
 {
-	public String message;
-	
-	public MoodAnalyserService() 
-	{
-		super();
-	}
-
-	public MoodAnalyserService(String message) 
-	{
-		super();
-		this.message = message;
-	}
-	
-	public Object analyseMood()
-	{
-		return analyseMood(message);
-	}
-
-	public Object analyseMood(String message)
+	public String analyseMood(String message) 
 	{
 		if(message.contains("sad"))
 		{
@@ -31,5 +13,4 @@ public class MoodAnalyserService
 			return "happy";
 		}
 	}
-
 }
